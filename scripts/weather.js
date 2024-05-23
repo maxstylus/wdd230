b654eddaaf3e2ada590152f59089898c9f3509a8
Lauren Snyder <lauren@topwebworks.com>
2024-05-23 14:08:30 -0700

    weather via api and font resizing

---
+++ scripts/weather.js
@@ -0,0 +1,45 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+let api_key = 'c6729828dbc30c929bc2b84f68ef45c5';
+let lat = 33.44;
+let lon = -112.07;
+let units = 'imperial';
+
+const url = `https://api.openweathermap.org/data/2.5/weather?units=${units}&lat=${lat}&lon=${lon}&appid=${api_key}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); //For testing only
+            displayResults(data);
+
+        } else {
+            throw Error(await response.text());
+        }
+      } catch (error) {
+        console.error(error);
+      }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    console.log(currentTemp.innerHTML);
+
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    let desc = data.weather[0].description;
+    
+    console.log(desc); //testing
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', `${desc}`); // fill in the blank
+
+    captionDesc.textContent = `${desc}`;
+
+}
