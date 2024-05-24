db39592fc52ed03cc7a85bd0703a6f9d4dac8c8c
Lauren Snyder <lauren@topwebworks.com>
2024-05-23 22:46:44 -0700

    dynmaic  activities list

--- scripts/links.js
+++ scripts/links.js
@@ -6,23 +6,34 @@ async function getLinks() {
     const data = await response.json();
     console.log(data); // Testing only
 
-    displayLinks(data);
+    displayLinks(data.weeks);
   }
 
+  getLinks();
+
   function displayLinks(weeks) {
-    let linkMenu = document.querySelector("ul");
-    weeks.weeks.forEach((week) => {
+    let linkMenu = document.querySelector("section.card.activities ul");
+
+    weeks.forEach((week) => {
         let listElement = document.createElement("li");
         listElement.textContent = `${week.week}: `;
+
+        let space = document.createElement("br");
+
         linkMenu.appendChild(listElement);
+        linkMenu.appendChild(space);
+
 
         week.links.forEach((link) => {
             let linkElement = document.createElement("a");
+
             linkElement.setAttribute('href', link.url);
-            linkElement.textContent = link.title;
+            linkElement.textContent = ` ${link.title} |`;
 
             listElement.appendChild(linkElement);
+            
         });    
+        
     });
   }
   
