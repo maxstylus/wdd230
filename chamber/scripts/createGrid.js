5bcd0ddc3bf51bee2b209cf846adfcc0793d15ef
Lauren Snyder <lauren@topwebworks.com>
2024-06-03 19:37:13 -0700

    html error fixes footer icon updates creategrid javascript updates

--- chamber/scripts/createGrid.js
+++ chamber/scripts/createGrid.js
@@ -1,18 +1,17 @@
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+document.addEventListener("DOMContentLoaded", () => {
+    const gridbutton = document.querySelector("#grid");
+    const listbutton = document.querySelector("#list");
+    const display = document.querySelector("article");
 
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+    // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
+    gridbutton.addEventListener("click", () => {
+        display.classList.add("grid");
+        display.classList.remove("list");
+    });
+
+    listbutton.addEventListener("click", () => {
+        display.classList.add("list");
+        display.classList.remove("grid");
+    });
 });
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}
