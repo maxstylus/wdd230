c565e4e83f89246a07076600ed0ab7b7f5425b21
Lauren Snyder <lauren@topwebworks.com>
2024-06-05 19:09:32 -0700

    Updated visit.js and added a transform on hover

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,23 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    /*
+    If this is the user's first visit, display "Welcome! Let us know if you have any questions.".
+    If the amount of time between visits is less than a day, display "Back so soon! Awesome!".
+    Otherwise, display the number of days in a message like this: "You last visited n days ago.", where n is the actual, whole number of days between visits. If the number of days is 1, then change the language to "day" not "days".
+    */
+
+
+    window.addEventListener("load", () => {
+        console.log("The entire page has loaded");
+        const discoverPageVisitContainer = document.getElementById('discover-pg-visits');       
+
+        const visits = document.createElement('p');
     
-    function storeCurrentTime() {
-        // Get the current date and time in milliseconds
         const currentTime = Date.now();
+        const lastTime = getStoredTime();
+        const twentyFourHoursFromNow = currentTime + 24 * 60 * 60 * 1000;
+        const daysFromLastVisit = calculateDaysDifference(Number(lastTime), currentTime);;
 
-        // Store the current date and time in localStorage
-        localStorage.setItem('currentTime', currentTime);
+        if (lastTime === null) {
+            visits.textContent = "";
+            visits.textContent = "Welcome! Let us know if you have any questions.";
 
-        // Retrieve and display the stored value
-        const storedTime = localStorage.getItem('currentTime');
-        console.log(`Stored Time: ${storedTime}`);
+        } else if ((lastTime - currentTime) < twentyFourHoursFromNow) {
+            visits.textContent = "";
+            visits.textContent = "Back so soon! Awesome!";
+        } else {
+            visits.textContent = "";
+            visits.textContent = `You last visited ${daysDifference.toFixed(2)} days ago.`; //parseStr(${daysFromLastVisit})
+        }
 
-        // Convert the stored time back to a Date object if needed
-        const date = new Date(parseInt(storedTime));
-        console.log(`Stored Time as Date: ${date}`);
+        setVisitTime(currentTime);
+        discoverPageVisitContainer.appendChild(visits);
+        
+    });
+
+    function setVisitTime(time) {
+        // Set current date and time in localStorage
+        localStorage.setItem('currentTime', time);
     }
 
-    // Call the function on page load
-    window.onload = storeCurrentTime;
+    function getStoredTime() {
+        // Retrieve and display the stored value
+        return new Date(parseInt(localStorage.getItem('currentTime')));        
+    }
+
+    function calculateDaysDifference(previousTimestamp, currentTimestamp) {
+        const msInADay = 24 * 60 * 60 * 1000;
+        return (currentTimestamp - previousTimestamp) / msInADay;
+    }
 
 });
